61682e88e84566f460b6ab736cb052bc04ab5644
grant.johnsong <grant.johnsong@d24a9aae-06bd-11de-a3aa-254263d3d64b>
2009-03-04 22:30:37 +0000

    Added movieProcessor.cpp

---
+++ movieProcessor.cpp
@@ -0,0 +1,15 @@
+#include "p3.h"
+
+void MovieProcessor::ProcessMovie(class Movie m) {
+	vector<class TreeNode*> node_list;
+	TreeNode *min;
+}
+
+
+class BaconTree MovieProcessor::ProcessInput() {
+
+}
+
+MovieProcessor::MovieProcessor(string inputFile) {
+	p = new Parser(inputFile);
+}
