9843739751f953a088425d2e67081525cb9dbcd5
grant.johnsong <grant.johnsong@d24a9aae-06bd-11de-a3aa-254263d3d64b>
2009-03-11 02:08:30 +0000

    comments added for p3.h
    must add comments for MovieProcessor class!

--- levelBylevel/p3.h
+++ levelBylevel/p3.h
@@ -8,7 +8,17 @@
 
 using namespace std;
 
-
+/*	a single node in the bacon tree
+ *
+ * 	Members:
+ * 		actorName - name of actor represented
+ * 		baconNumber - the number of degrees separating this actor from Kevin Bacon
+ * 		movieName - the movie the represented actor was in with its parent
+ *
+ * 	Methods:
+ * 		TreeNode - constructor
+ *
+ */
 class TreeNode {
 public:
     friend class BaconTree;
@@ -24,8 +34,22 @@ public:
     }
 };
 
-// List of Movies Objects (each containing a list of actors)
-
+/*	tree connecting actors to Kevin Bacon
+ *	Each level of the tree contains all of the actors a number of degrees from Kevin Bacon.
+ *	The tree is rooted at Kevin Bacon, and lower levels are progressively further away.
+ *
+ *	Members:
+ *		root - the root of the tree
+ *		actor_hash - not really a hash, it maps actor names to their representative tree nodes
+ *		levelList - a list of lists of TreeNode pointers. levelList[n] contains all of the TreeNodes at level n in the tree.
+ *
+ *	Methods:
+ *		AddTreeNode - add a given TreeNode to the tree under given parent already in tree
+		AddActor - create a TreeNode from given actor, movie and add it to the tree
+ *		IsActorInTree - check if the tree contains an actor
+ *		getTreeNode - get TreeNode in tree representing a given actor
+ *
+ */
 class BaconTree {
 public:
     TreeNode *root; //bacon# 0
@@ -39,6 +63,21 @@ public:
     BaconTree(); 
 };   
 
+/* 	representation of a single movie and its cast
+ *
+ * 	Members:
+ * 		movieName - name of movie
+ * 		isKnown - has the movie been added to the tree?
+ * 		actorNames - names of the actors in the movie
+ *
+ * 	Methods:
+ * 		Movie - constructor. Creates a new movie with given name
+ * 		actorIn - check if a given actor is in the movie
+ * 		insertActor - add a given actor to the movie
+ * 		removeActor - remove a given actor from the movie
+ * 		popActor - remove an arbitrary actor from the movie and return his/her name
+ *
+ */
 class Movie {
 public:
     string movieName;
@@ -50,18 +89,48 @@ public:
     void insertActor(string actor);
     void removeActor(string actor);
     string popActor();
-    int length();
 };
 
+/*	reads input file and creates class Movie objects
+ *
+ *	Members:
+ *		inputstream - strem used to read the input file.
+ *
+ *	Methods:
+ *		Parser - constructor. Creates a parser to read a given input file
+ *		getNextMovie - read next movie from the input and create and return a class Movie object representing it.
+ *
+ */
 class Parser {
 public:
     ifstream *inputstream;
     
     Parser(string inputFile);
-    void RestartInput(string inputFile);
     class Movie* getNextMovie();
 };
 
+/*	translates input file to a bacon tree
+ *
+ *	Members:
+ *		private:
+ *			pos - TODO
+ *			it - TODO
+ *			isDone - TODO
+ *			unknownMovies - TODO
+ *			atEnd - TODO
+ *			p - parser to read input file
+ *			bacontree - tree created from input file
+ *			
+ *	Methods:
+ *		private:
+ *			ProcessMovie - TODO ...a given movie
+ *
+ *		public:
+ *			ProcessInput - TODO
+ *			PrintBaconChain - output the relationship of a given actor to Kevin Bacon
+ *			ProcessTreesNmovies - TODO
+ *
+ */
 class MovieProcessor {
 private:
     int pos; int it;
